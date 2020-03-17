660f7b9cc90ee341c06659ee83268d8f59e1b7b1
prashant325 <62289395+prashant325@users.noreply.github.com>
2020-03-17 23:52:23 +0530

    Create Check_the_given_number_is_Even_or_Odd.c
    UNIQUE & Such a well-written program will not be found anywhere on your entire Google.

---
+++ Check_the_given_number_is_Even_or_Odd.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int number;
+printf("Enter the number to check 'Even' or 'Odd' >--->> ");
+scanf("%d",&number);
+if(number%2==0)
+printf("The entered number is 'EVEN' ");
+
+else
+printf("Entered number is 'ODD' "); 
+return 0;
+}
+
+
+
+
